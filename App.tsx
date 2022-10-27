66f5c1d37b56103bba01f8751cc8ae6f225a757d
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-10-27 14:35:38 -0300

    Configurando StatusBar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
-  if (!fontsLoaded) {
-    return (
-      <View>
-      <Text>Loading</Text>
-    </View>
-    )
-  }
-
   return (
     <View>
-      <Text>Hello world</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {!fontsLoaded ? (<Text>Loading</Text>) : (<Text>Hello world</Text>)}
     </View>
   );
 }
