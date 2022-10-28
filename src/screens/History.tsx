2c6399048c7a423ccfcb953bfe8dc7a8a77d2e24
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-10-28 13:39:29 -0300

    Histórico desenvolvido

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,32 @@
-import { Center, Text } from "native-base"
+import { useState } from "react"
+import { Heading, VStack, SectionList } from "native-base"
+
+import { HistoryCard } from "@components/HistoryCard"
+import { ScreenHeader } from "@components/ScreenHeader"
 
 export function History() {
+  const [exercises, setExercises] = useState([
+    { title: '12.12.2022', data: ['Teste', 'Teste2'] }, 
+    { title: '13.12.2022', data: ['Teste3', 'Teste4'] }
+  ])
+
   return (
-    <Center flex={1}>
-      <Text color="white">History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderSectionHeader={({ section: { title } }) => (
+          <Heading color="gray.300" fontSize="md" mt={10} mb={3}>
+            {title}
+          </Heading>
+        )}
+        renderItem={({ item }) => <HistoryCard />}
+
+        showsVerticalScrollIndicator={false}
+        px={8}
+      />
+    </VStack>
   )
 }
