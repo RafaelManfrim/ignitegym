35259b5ccc93833c0fd7ffb71249829242c9a653
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-10-28 14:10:08 -0300

    Exibição da imagem de perfil do usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,42 @@
-import { Center, Text } from "native-base"
+import { ScreenHeader } from "@components/ScreenHeader"
+import { UserPhoto } from "@components/UserPhoto"
+import { Center, ScrollView, Skeleton, Text, VStack } from "native-base"
+import { useState } from "react"
+import { TouchableOpacity } from "react-native"
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton 
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.400"
+              endColor="gray.500"
+            />
+          ) : (
+            <UserPhoto 
+              source={{ uri: "https://github.com/rafaelmanfrim.png" }} 
+              size={PHOTO_SIZE}
+              alt="Foto do usuário"
+            />
+          )}
+
+          <TouchableOpacity>
+            <Text color="green.500" fontSize="md" mb={8} mt={2} fontWeight="bold">
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
